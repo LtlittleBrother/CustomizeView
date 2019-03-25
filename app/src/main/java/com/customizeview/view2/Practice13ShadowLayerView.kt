45d21036f84dc76c257570e7061451a3781884ab
LittlBrother <2061094699@qq.com>
2019-03-25 16:53:01 +0800

    style : style

--- app/src/main/java/com/customizeview/view2/Practice13ShadowLayerView.kt
+++ app/src/main/java/com/customizeview/view2/Practice13ShadowLayerView.kt
@@ -1,11 +1,34 @@
 package com.customizeview.view2
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
+/**
+ * setShadowLayer(float radius, float dx, float dy, int shadowColor)
+ * 在之后的绘制内容下面加一层阴影。
+ *
+ * 注意：
+ * 如果要清除阴影层，使用 clearShadowLayer() 。
+ * 在硬件加速开启的情况下， setShadowLayer() 只支持文字的绘制，文字之外的绘制必须关闭硬件加速才能正常绘制阴影。
+ * 如果 shadowColor 是半透明的，阴影的透明度就使用 shadowColor 自己的透明度；而如果  shadowColor 是不透明的，阴影的透明度就使用 paint 的透明度。
+ * */
 class Practice13ShadowLayerView: View {
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    @SuppressLint("DrawAllocation")
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val paint = Paint()
+        paint.textSize = 100f
+        paint.color = Color.RED
+        paint.setShadowLayer(10f,0f,0f,Color.BLUE)
+        canvas!!.drawText("setShadowLayer",100f,100f,paint)
+    }
 }
--- app/src/main/java/com/customizeview/view2/Practice14MaskFilterView.kt
+++ app/src/main/java/com/customizeview/view2/Practice14MaskFilterView.kt
@@ -1,11 +1,51 @@
 package com.customizeview.view2
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import com.customizeview.R
+import android.graphics.EmbossMaskFilter
+
+
+
+/**
+ * setMaskFilter(MaskFilter maskfilter)
+ * 为之后的绘制设置 MaskFilter。上一个方法 setShadowLayer() 是设置的在绘制层下方的附加效果；而这个 MaskFilter 和它相反，设置的是在绘制层上方的附加效果。
+ * MaskFilter 有两种：
+ * BlurMaskFilter ：
+ * 模糊效果的 MaskFilter
+ * 它的构造方法 BlurMaskFilter(float radius, BlurMaskFilter.Blur style) 中， radius 参数是模糊的范围， style 是模糊的类型。一共有四种：
+ * NORMAL: 内外都模糊绘制
+ * SOLID: 内部正常绘制，外部模糊
+ * INNER: 内部模糊，外部不绘制
+ * OUTER: 内部不绘制，外部模糊（什么鬼？）
+ *
+ *
+ * EmbossMaskFilter ：
+ * 浮雕效果的 MaskFilter
+ * 它的构造方法  EmbossMaskFilter(float[] direction, float ambient, float specular, float blurRadius) 的参数里，
+ * direction 是一个 3 个元素的数组，指定了光源的方向；
+ * ambient 是环境光的强度，数值范围是 0 到 1；
+ * specular 是炫光的系数；
+ * blurRadius 是应用光线的范围。
+ * */
 
 class Practice14MaskFilterView: View {
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    @SuppressLint("DrawAllocation")
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        val paint = Paint()
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.dianyou_red)
+        paint.maskFilter = BlurMaskFilter(50f,BlurMaskFilter.Blur.NORMAL)
+        canvas!!.drawBitmap(bitmap,100f,100f,paint)
+
+        paint.maskFilter = EmbossMaskFilter(floatArrayOf(0f, 3f, 3f), 0.8f, 8f, 10f)
+        canvas.drawBitmap(bitmap,400f,100f,paint)
+    }
 }
